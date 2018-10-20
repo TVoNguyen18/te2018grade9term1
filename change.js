fc03c47c081303f3c6ea71be2d9cb1accd6b81b1
Thanhthanh Nguyen <thanhthanh@csedge.org>
2018-10-20 10:30:30 -0700

    Modified files

--- change.js
+++ change.js
@@ -1,7 +1,72 @@
-// import readline-sync module, don't touch!
-const READLINE = require("readline-sync");
+const READLINE = require('readline-sync');
 
-// fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
+//let userInput = READLINE.question("Type anything and I'll repeat it back!");
+//console.log(userInput);
 
-// write code below that simulates the change machine in action!
+// declare and assign a variable for pennies using readlineSync
+// ask the user how many pennies he/she wants change for
+
+let numOfPennies = READLINE.question("Please enter cents as a positive integer: ");
+console.log(numOfPennies);
+
+// declare, assign, and print how many quarters you can make out of the pennies
+
+//let quarters = Math.trunc(pennies / 25);
+//quarters = Math.trunc(quarters);
+//console.log(quarters);
+
+let numOfQuarters = Math.floor(numOfPennies / 25);
+console.log(numOfQuarters, "QUARTERS");
+
+
+let remainder = numOfPennies % 25;
+
+// declare, assign, and print how many dimes you can make out of the pennies
+//let dimes = Math.trunc(pennies / (pennies - (quarters * 25)));
+//console.log(dimes);
+
+let numOfDimes = Math.floor(remainder / 10);
+console.log(numOfDimes, "DIMES");
+
+remainder = remainder % 10;
+
+
+// declare, assign, and print how many nickels you can make out of the pennies
+//let nickels = pennies -
+
+let numOfNickels = Math.floor(remainder / 5);
+console.log(numOfNickels, "NICKELS");
+
+remainder = remainder % 5;
+
+
+// declare, assign, and print how many pennies are left over
+
+let finalPennies = Math.floor(remainder / 1);
+console.log(finalPennies, "PENNIES");
+
+
+
+// make a change drawer using an object to
+// store all of your change
+// each property on the object should represent the particular coin
+// use the dot operator on the object to assign how many of each coin you have
+// many coins are in the drawer
+// print the drawer to see that the values are correct
+
+let changeDrawer = {
+  quarters: undefined,
+  dimes: undefined,
+  nickels: undefined,
+  pennies: undefined
+};
+
+changeDrawer.quarters = numOfQuarters;
+changeDrawer.dimes = numOfDimes;
+changeDrawer.nickels = numOfNickels;
+changeDrawer.pennies = finalPennies;
+
+console.log(`${changeDrawer.quarters}
+${changeDrawer.dimes}
+${changeDrawer.nickels}
+${changeDrawer.pennies}`);
---
+++ objects.js
@@ -0,0 +1,15 @@
+let somajMoore = {
+  currentYear: 2018,
+  homePlanet: "Earth",
+  isHighSchoolStudent: true,
+  creditCardNumber: undefined,
+  firstName: "Somaj",
+  nickname: "Maj",
+  numberOfSiblings: 2,
+  catchPhrase: "Spicy",
+  hasNintendoSwitch: false,
+  hasSmartphone: true,
+  middleInitial: "T"
+};
+//numberOfSiblings++;
+console.log(somajMoore);
