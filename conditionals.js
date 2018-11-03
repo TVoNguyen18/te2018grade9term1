b480fd28aef2891922084225cb715000a6e82099
Thanhthanh Nguyen <thanhthanh@csedge.org>
2018-11-03 15:50:35 -0700

    update to master

--- conditionals.js
+++ conditionals.js
@@ -1,80 +1,80 @@
 const READLINE = require("readline-sync");
-//
-// // **** Problem 1: R-rated ****
-// // You cannot see an R-rated movie unless you are at least 18, or you are with
-// // an adult. Write code that prints whether or not someone can see an
-// // R-rated movie.
-// console.log("*** Problem 1: R-rated ***");
-// let age = READLINE.question("Enter your age: ");
-//
-// if (age < 18) {
-//   console.log ("You are under 18. You cannot watch this movie without an adult.");
-//   let withAdult = READLINE.question("Are you with an adult? (yes or no): ");
-//   if (withAdult === "no") {
-//     console.log("You are under 18 without an adult. You cannot watch this movie.");
-//   } else {
-//     console.log("Why did your adult let you watch this...?");
-//   }
-// } else {
-//   console.log("Have fun at the movies! Heh heh heh...");
-// }
-//
-// // **** Problem 2: Umbrella ****
-// // You should bring an umbrella when you travel, but only if it is raining.
-// // However, if it is thunderstorming, you should not bring an umbrella since
-// // that's bad luck. Write code that prints if someone should bring an umbrella
-// // with them.
-// console.log("*** Problem 2: Umbrella ***");
-// let raining = READLINE.question("Is it raining? (yes or no): ");
-// if (raining === "yes") {
-//   console.log("You should bring an umbrella.");
-//   let thunderstorming = READLINE.question("Is it thunderstorming? (yes or no): ");
-//   if (thunderstorming === "yes") {
-//     console.log("You should not bring an umbrella, because that's bad luck.");
-//   } else {
-//     console.log("Don't forget the umbrella!");
-//   }
-// } else {
-//   console.log("Lucky!");
-// }
-//
-// // **** Problem 3: Monkey Trouble ****
-// // There are two monkeys: Bubbles and Spankey. You are in trouble if both of
-// // them are smiling, or if neither are smiling. Write code that prints if we
-// // are in trouble.
-// console.log("*** Problem 3: Monkey Trouble ***");
-// let bubbles = READLINE.question("Is Bubbles smiling? (yes or no): ");
-// let spankey = READLINE.question("Is Spankey smiling? (yes or no): ");
-//
-// if ((bubbles === "yes" && spankey === "yes") ||
-//   (bubbles === "no" && spankey === "no")) {
-//   console.log("We are in trouble. Why, exactly?");
-// } else {
-//   console.log("Whew!");
-// }
+
+// **** Problem 1: R-rated ****
+// You cannot see an R-rated movie unless you are at least 18, or you are with
+// an adult. Write code that prints whether or not someone can see an
+// R-rated movie.
+console.log("*** Problem 1: R-rated ***");
+let age = READLINE.question("Enter your age: ");
+
+if (age < 18) {
+  console.log ("You are under 18. You cannot watch this movie without an adult.");
+  let withAdult = READLINE.question("Are you with an adult? (yes or no): ");
+  if (withAdult === "no") {
+    console.log("You are under 18 without an adult. You cannot watch this movie.");
+  } else {
+    console.log("Why did your adult let you watch this...?");
+  }
+} else {
+  console.log("Have fun at the movies! Heh heh heh...");
+}
+
+// **** Problem 2: Umbrella ****
+// You should bring an umbrella when you travel, but only if it is raining.
+// However, if it is thunderstorming, you should not bring an umbrella since
+// that's bad luck. Write code that prints if someone should bring an umbrella
+// with them.
+console.log("*** Problem 2: Umbrella ***");
+let raining = READLINE.question("Is it raining? (yes or no): ");
+if (raining === "yes") {
+  console.log("You should bring an umbrella.");
+  let thunderstorming = READLINE.question("Is it thunderstorming? (yes or no): ");
+  if (thunderstorming === "yes") {
+    console.log("You should not bring an umbrella, because that's bad luck.");
+  } else {
+    console.log("Don't forget the umbrella!");
+  }
+} else {
+  console.log("Lucky!");
+}
+
+// **** Problem 3: Monkey Trouble ****
+// There are two monkeys: Bubbles and Spankey. You are in trouble if both of
+// them are smiling, or if neither are smiling. Write code that prints if we
+// are in trouble.
+console.log("*** Problem 3: Monkey Trouble ***");
+let bubbles = READLINE.question("Is Bubbles smiling? (yes or no): ");
+let spankey = READLINE.question("Is Spankey smiling? (yes or no): ");
+
+if ((bubbles === "yes" && spankey === "yes") ||
+  (bubbles === "no" && spankey === "no")) {
+  console.log("We are in trouble. Why, exactly?");
+} else {
+  console.log("Whew!");
+}
 
 
-// **** Problem 4: First Place ****
-// Write code that prints the largest of three scores. If there is a tie for
-// first place, simply print one of the tie winners.
-// console.log("*** Problem 4: First Place ***");
-// let score1 = READLINE.question("Enter the first score: ");
-// let score2 = READLINE.question("Enter the second score: ");
-// let score3 = READLINE.question("Enter the third score: ");
-//
-// if (score1 >= score2 && score1 >= score3) {
-//   console.log(score1, '^^^^^');
-//   console.log(score2, '*****');
-//   console.log(score3, '&&&&&');
-//   // console.log("++++++++");
-// } else if (score2 >= score1 && score2 >= score3) {
-//   console.log(score2);
-//   // console.log("*****");
-// } else if (score3 >= score1 && score3 >= score2) {
-//   console.log(score3);
-// } else {
-//   console.log("I do not know what I am doing...");
-// }
+**** Problem 4: First Place ****
+Write code that prints the largest of three scores. If there is a tie for
+first place, simply print one of the tie winners.
+console.log("*** Problem 4: First Place ***");
+let score1 = READLINE.question("Enter the first score: ");
+let score2 = READLINE.question("Enter the second score: ");
+let score3 = READLINE.question("Enter the third score: ");
+
+if (score1 >= score2 && score1 >= score3) {
+  console.log(score1, '^^^^^');
+  console.log(score2, '*****');
+  console.log(score3, '&&&&&');
+  // console.log("++++++++");
+} else if (score2 >= score1 && score2 >= score3) {
+  console.log(score2);
+  // console.log("*****");
+} else if (score3 >= score1 && score3 >= score2) {
+  console.log(score3);
+} else {
+  console.log("I do not know what I am doing...");
+}
 
 
 // **** Problem 5: Phone Shopping ****
--- haunted.js
+++ haunted.js
@@ -7,25 +7,83 @@ let player = {
   intellect: 0,
   strength: 0,
   charm: 0,
-  weapon: "fists",
-  item: "flashlight",
+  weapon: undefined,
+  item: "magic anime portal",
 };
 
-console.log();
-console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
-player.name = READLINE.question("What is your name?: ");
-console.log(`${player.name} stands in front of a spooky haunted house.`);
-console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
-let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
-if(enterHouse == "y" || enterHouse == "yes") {
-  player.courage++;
-  console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
-  // continue the story
+// console.log();
+// console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
+// player.name = READLINE.question("What is your name?: ");
+// console.log(`${player.name} stands in front of a spooky haunted house.`);
+// console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
+// let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
+// if(enterHouse == "y" || enterHouse == "yes") {
+//   player.courage++;
+//   console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
+//   // continue the story
+//
+// } else {
+//   player.intellect++;
+//   console.log(`${player.name} decides not to open the door. However...`);
+//   // continue the story
+//
+// }
+// console.log("Thanks for playing!");
+
+console.log("A magical anime portal has appeared in your hands.");
+player.name = READLINE.question("What is your name? ");
+console.log("Okay. " + player.name + ", choose which anime to teleport to!");
+let playerAnswer = READLINE.question("Do you choose The Devil is a Part-Timer! (1) or Black Butler (2)? ");
+
+if (playerAnswer == 1) {
+  console.log(player.name + "chooses The Devil is a Part Timer!");
+  console.log("You meet Maou Sadao!");
+  playerAnswer = READLINE.question("Do you scream your head off (1), or do you challenge him to a fight? (2)? ");
+
+  if (playerAnswer == 1) {
+    console.log("He is politely weirded out by your enthusiasm and invites you to his fast food restaurant joint");
+    playerAnswer = READLINE.question("Do you get the black pepper fries (1) or just sit there (2)? ");
+
+    if (playerAnswer == 1) {
+      console.log("The fryer broke...");
+      playerAnswer = READLINE.question("Do you get Maou to fix it with magic (1) or get coffee instead (2)? ");
+
+      if (playerAnswer == 1) {
+        console.log("He fixed it! But it will be your fault if he has no magic to fight his enemies...");
+      } else if (playerAnswer == 2) {
+        console.log("The coffee was pretty good...");
+      } else {
+        console.log("You are not doing this right...");
+      }
+    } else if (playerAnswer == 2) {
+      playerAnswer = READLINE.question("You have to order! Iced coffee (1) or apple pie (2)? ");
+    } else {
+      console.log("You are not doing this right!");
+    }
+  } else if (playerAnswer == 2) {
+    console.log("You hold the stolen sword Better Half to his face.");
+    playerAnswer = READLINE.question("Do you fight (1) or chicken out (2)? ");
+
+    if (playerAnswer == 1) {
+      console.log("Darn! The Demon Generals has arrived to fight you!");
+    } else if (playerAnswer == 2) {
+      console.log("You got trounced for being ridiculous...");
+    } else {
+      console.log("You are not doing this right!");
+    }
+  } else {
+    console.log("You are not doing this right!");
+  }
+} else if (playerAnswer == 2) {
+  console.log(player.name + " chooses Black Butler!");
+  playerAnswer = READLINE.question("You meet Ciel Phantomhive. Do you scream (1) or be haughty (2)? ");
+
+  if (playerAnswer == 1) {
+    console.log("You get kicked out of the house by Ciel.");
+  }
 
 } else {
-  player.intellect++;
-  console.log(`${player.name} decides not to open the door. However...`);
-  // continue the story
-
+  console.log("You are not doing this right!");
 }
-console.log("Thanks for playing!");
+
+console.log("The portal malfunctions and you return to real life, where a zombie apocalypse is happening all around you!");
