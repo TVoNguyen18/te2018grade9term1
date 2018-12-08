f4588aabcb0cd1cfabe4de7cb41fb0e9544acc0c
Thanhthanh <thanhthanh@csedge.org>
2018-12-08 11:03:59 -0800

    Update arraysAndStrings.js

--- arraysAndStrings.js
+++ arraysAndStrings.js
@@ -1,11 +1,36 @@
 let jordans = ['Jordan 13s', 'Jordans 5s', 'Jordan 10s', 'Jordan 7s'];
+
 // print out first pair of Jordans I found
 
-console.log(jordans[0]);
+// console.log(jordans[0]);
+//
+// console.log(jordans.length);
+
+console.log(jordans.toString());
+
+
+
+
+
+
+
+
+
+
 
 
 // strings
 
-let name = 'Donavan';
-
-console.log(name[0]);
+// let name = 'Donavan';
+//
+// console.log(name[0]);
+//
+// console.log(name.charAt(6));
+//
+// console.log(name.length);
+//
+// console.log(name.toUpperCase());
+//
+// console.log(name.toLowerCase());
+//
+// console.log(name.startsWith('D'));
