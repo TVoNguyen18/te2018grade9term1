71cba66c9f48b8666621c6b48fdd624a139b953c
Thanhthanh <thanhthanh@csedge.org>
2018-12-08 14:32:41 -0800

    Update whileAndForLoops.js

--- whileAndForLoops.js
+++ whileAndForLoops.js
@@ -1,11 +1,11 @@
-// let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-//
-// let i = 0;
-//
-// while (i < 26) {
-//   console.log(alphabet[i]);
-//   i++;
-// }
+let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+let i = 0;
+
+while (i < alphabet.length) {
+  console.log(alphabet[i]);
+  i++;
+}
 
 let num = 0;
 
