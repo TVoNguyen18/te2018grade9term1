a5bbe050885f4eee6afd7598e176db378af7959c
Thanhthanh <thanhthanh@csedge.org>
2018-12-08 14:21:40 -0800

    Create whileAndForLoops.js

---
+++ whileAndForLoops.js
@@ -0,0 +1,15 @@
+// let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//
+// let i = 0;
+//
+// while (i < 26) {
+//   console.log(alphabet[i]);
+//   i++;
+// }
+
+let num = 0;
+
+while (num < 5) {
+  console.log(num);
+  num++;
+}
