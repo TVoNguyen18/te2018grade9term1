fe482486f51ff72328fbe88e2dbc49deeefc9bcf
Thanhthanh <thanhthanh@csedge.org>
2018-12-08 10:53:25 -0800

    Stuff

---
+++ arraysAndStrings.js
@@ -0,0 +1,11 @@
+let jordans = ['Jordan 13s', 'Jordans 5s', 'Jordan 10s', 'Jordan 7s'];
+// print out first pair of Jordans I found
+
+console.log(jordans[0]);
+
+
+// strings
+
+let name = 'Donavan';
+
+console.log(name[0]);
